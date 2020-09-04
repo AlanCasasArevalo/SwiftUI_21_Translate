4fdfbceac440ab29facc1816b2619307f33b11b7
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-04 08:55:50 +0200

    [FEATURE][LANGUAGES] * Alert was added and translated

--- SwiftUI_21_Translate/ContentView.swift
+++ SwiftUI_21_Translate/ContentView.swift
@@ -3,9 +3,24 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var alert = false
+    
     var body: some View {
-        Text("title")
-            .font(.system(.largeTitle, design: .rounded))
+        VStack {
+            Text("title")
+                .font(.system(.largeTitle, design: .rounded))
+            
+            Button(action: {
+                self.alert.toggle()
+            }) {
+                Text("content_view_button")
+            }
+            .alert(isPresented: self.$alert) { () -> Alert in
+                Alert(title: Text("content_view_alert_title"), message: Text("content_view_alert_message"), dismissButton:
+                    .default(Text("content_view_alert_dismiss_button"))
+                )
+            }
+        }
     }
 }
 
